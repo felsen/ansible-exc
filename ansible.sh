d756653ca08ff6f30da77e63d417b62b40090250
root <root@localhost.localdomain>
2018-01-04 10:48:29 +0545

    Modified: changes done for after 100 for OMSDK

--- ansible.sh
+++ ansible.sh
@@ -10,10 +10,10 @@ if [[ ("$1" != "") && ("$2" != "") ]]; then
     python3.6 install.py
     cd ../
     rm -rf "DellEMC_OpenManage_Ansible_Modules_version_1.0.$1.tar.gz"
-    pip2.7 install "http://bdcbldhttpnas.blr.amer.dell.com/ombuilds$/OMSDK/1.0/$2/omdrivers-0.2.00$2-py2.py3-none-any.whl"
-    pip2.7 install "http://bdcbldhttpnas.blr.amer.dell.com/ombuilds$/OMSDK/1.0/$2/omsdk-0.2.00$2-py2.py3-none-any.whl"
-    pip3.6 install "http://bdcbldhttpnas.blr.amer.dell.com/ombuilds$/OMSDK/1.0/$2/omdrivers-0.2.00$2-py2.py3-none-any.whl"
-    pip3.6 install "http://bdcbldhttpnas.blr.amer.dell.com/ombuilds$/OMSDK/1.0/$2/omsdk-0.2.00$2-py2.py3-none-any.whl"
+    pip2.7 install "http://bdcbldhttpnas.blr.amer.dell.com/ombuilds$/OMSDK/1.0/$2/omdrivers-0.2.0$2-py2.py3-none-any.whl"
+    pip2.7 install "http://bdcbldhttpnas.blr.amer.dell.com/ombuilds$/OMSDK/1.0/$2/omsdk-0.2.0$2-py2.py3-none-any.whl"
+    pip3.6 install "http://bdcbldhttpnas.blr.amer.dell.com/ombuilds$/OMSDK/1.0/$2/omdrivers-0.2.0$2-py2.py3-none-any.whl"
+    pip3.6 install "http://bdcbldhttpnas.blr.amer.dell.com/ombuilds$/OMSDK/1.0/$2/omsdk-0.2.0$2-py2.py3-none-any.whl"
 else
     echo "Error: Command line argument should be ./ansible.sh ansible_build_no omsdk_build_no."
 fi
